9afdcefee4834a3bfdaab5a25106bb2d7ebfc62f
Greydius <mr.greydius@gmail.com>
2020-08-22 16:41:48 +0500

    footer new logos

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,9 +1,21 @@
 <template>
   <a-layout-footer class="footer">
     <div class="footer__block">
+      <a href="http://admoblkaluga.ru/sub/ecology/" class="footer__link">
+        <img src="@/assets/images/footer-logo-0.png" class="footer__logo" alt="">
+      </a>
+      
+    </div>
+
+    <div class="footer__block">
       <img src="@/assets/images/footer-logo-1.png" class="footer__logo" alt="">
       <img src="@/assets/images/footer-logo-2.png" class="footer__logo" alt="">
-      <!-- <img src="@/assets/images/footer-logo-3.png" class="footer__logo" alt=""> -->
+      <img src="@/assets/images/footer-logo-3.svg" class="footer__logo" alt="">
+      <img src="@/assets/images/footer-logo-4.svg" class="footer__logo" alt="">
+    </div>
+
+    <div class="footer__block">
+      <img src="@/assets/images/footer-logo-5.jpg" class="footer__logo" alt="">
     </div>
   </a-layout-footer>
 </template>
@@ -17,11 +29,22 @@ export default {
 <style lang="scss">
 .footer {
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 
   &__block {
+    flex: 1 33%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
+    &:first-child {
+      justify-content: flex-start;
+    }
+
+    &:last-child {
+      justify-content: flex-end;
+    }
   }
 
   &__logo {
