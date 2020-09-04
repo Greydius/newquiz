e2444ba95bcd5d74d84ac1b1c134b9a31855098e
Greydius <mr.greydius@gmail.com>
2020-09-05 00:24:44 +0500

    add AnswersSelect

---
+++ src/components/questions/AnswersSelect.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="answers-select-form-item__wrapper">
+    <h4 class="answers-select-form-item__title">{{ content.title }}</h4>
+    <a-form-item
+      class="answers-select-form-item"
+    >
+    <a-radio-group
+      v-decorator="[
+        `question[${index}]`,
+        { rules: [{ required: true, message: 'Обязательное поле!' }] },
+      ]"
+      class="answers-select-form-item__radio-group"
+    >
+      <a-radio-button
+        v-for="(answer) in content.answers"
+        :key="answer"
+        :value="answer"
+        class="answers-select-form-item__radio-button"
+      >
+        {{ answer }}
+      </a-radio-button>
+    </a-radio-group>
+    </a-form-item>   
+  </div>
+  
+</template>
+
+<script>
+import { Form, Radio } from 'ant-design-vue'
+
+export default {
+  props: {
+    content: Object,
+    index: Number,
+  },
+
+  components: {
+    'a-form-item': Form.Item,
+    'a-radio-group': Radio.Group,
+    'a-radio-button': Radio.Button,
+  }
+}
+</script>
+
+<style lang="scss">
+.answers-select-form-item {
+  &__image {
+    width: 100%;
+  }
+
+  &__title {
+    line-height: 1;
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+
+  &__description {
+    line-height: 1;
+    margin-bottom: 10px;
+  }
+}
+</style>
---
+++ src/components/questions/ImagesCompare.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="images-compare-form-item__wrapper">
+    <h4 class="images-compare-form-item__title">{{ content.title }}</h4>
+    <div class="images-compare-form-item__inner-wrapper">
+      <div class="images-compare-form-item__images-wrapper">
+        <div class="images-compare-form-item__images-inner-wrapper">
+          <div
+            v-for="(image, i) in content.images[0]"
+            :key="image"
+            class="images-compare-form-item__image-wrapper"
+          >
+            <img
+              :src="image"
+              alt=""
+              class="images-compare-form-item__image"
+            >
+            <div class="images-compare-form-item__image-title">{{ ab[i] }}</div>
+          </div>
+          
+        </div>
+        <div class="images-compare-form-item__images-inner-wrapper">
+          <div
+            v-for="(image, i) in content.images[0]"
+            :key="image"
+            class="images-compare-form-item__image-wrapper"
+          >
+            <img
+              :src="image"
+              alt=""
+              class="images-compare-form-item__image"
+            >
+            <div class="images-compare-form-item__image-title">{{ i + 1 }}</div>   
+          </div>
+        </div>
+        
+      </div>
+      <a-form-item
+        v-for="(image, i) in content.images[0]"
+        :key="image"
+        :label="`${ab[i]}`"
+        class="images-compare-form-item"
+      >
+        <a-input
+          v-decorator="[
+            `question[${index}][${i}]`,
+            { rules: [{ required: true, message: 'Обязательное поле!' }] },
+          ]"
+          placeholder="Введите ответ..."
+        >
+        </a-input>
+      </a-form-item>   
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import { Form, Input } from 'ant-design-vue'
+
+export default {
+  name: 'ImagesCompare',
+  props: {
+    content: Object,
+    index: Number,
+  },
+
+  components: {
+    'a-form-item': Form.Item,
+    'a-input': Input
+  },
+
+  data() {
+    return {
+      ab: ['А', 'Б', 'В', 'Г']
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.images-compare-form-item {
+  [class*="ant-form-item"] {
+    height: 100%;
+  }
+
+  .ant-form-item-children {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  &__images-inner-wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-column-gap: 30px;
+    grid-row-gap: 30px;
+    margin-bottom: 30px;
+  }
+
+  &__image-wrapper {
+    background-color: #fff;
+  }
+
+  &__image {
+    width: 100%;
+  }
+
+  &__image-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 5px 10px;
+    border-top: 1px solid rgb(0 0 0 / 35%);
+  }
+
+  &__title {
+    line-height: 1;
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+
+  &__description {
+    line-height: 1;
+    margin-bottom: 10px;
+  }
+}
+</style>
